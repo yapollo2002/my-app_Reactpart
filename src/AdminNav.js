84abc1bc5956aa280b4597db09b9ca30615fbca0
yapollo2002 <yapollo2002@yahoo.com>
2021-05-07 23:43:03 +0300

    First steps of creating AdminNav element (forms, handlers)

--- src/AdminNav.js
+++ src/AdminNav.js
@@ -2,13 +2,41 @@ import React, {Component} from 'react';
 
 
 export default class AdminNav extends Component {
+           constructor(props){
+        super(props);
+       this.state={
+           telnum: '',
+           errormessage: ''
+        };
+       }
+          myChangeHandler = (event) => {
+	  
+    let val = event.target.value;
+    let err = '';
+    
+      if (val !=="" && !Number(val)) {
+        err = <strong>Your age must be a number</strong>;
+      }
+    
+    this.setState({errormessage: err});
+	this.setState({telnum: val});
+  }
+     handle = (event)=> {
+		 event.preventDefault();
+		 this.props.handle(this.state.telnum);
+		 
+		 
+	 }
+
 	
 	render() {
 		return(
              <div className="AdminNav">
          <p> AdminNav </p>
          <form>    
+            <input type="text" name="search" onChange={this.myChangeHandler}/>
 
+            <input type="text" id="adminsub"  value="Пошук" onclic={this.hanle}/>
          </form> 
          </div>
                 )		
