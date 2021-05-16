8d086af899d59651553e011675655f6835e8fb08
yapollo2002 <yapollo2002@yahoo.com>
2021-05-16 10:49:33 +0300

    Confirm button has corrected its layout

--- src/ModifierOutput.js
+++ src/ModifierOutput.js
@@ -33,12 +33,20 @@ render(){
          <input type="text" name="issue"  value="issue" size='35'/>
          </div>
          
+         <div className="notesconfirm">
+         
          <div className="notesinput" >
-         <textarea type="text" name="notes"  value="notes" cols='60' rows='7' >
+         <textarea type="text" name="notes" id="notes" value="notes" cols='60' rows='7' >
          </textarea>
-         <button name="confirm" id="confirm" onClik={this.handler}/> 
          </div>
          
+         <div className="confirmbutton">
+         <button type="button" name="confirm" id="confirm" onclik={this.handler}>
+         Confirm
+         </button> 
+         </div>
+
+         </div>
 
 	</div>
 	
