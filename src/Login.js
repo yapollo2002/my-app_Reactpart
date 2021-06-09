e51d799810707a87fce2a1a0fd3caf7ba3f91cbc
yapollo2002 <yapollo2002@yahoo.com>
2021-06-09 23:46:32 +0300

    Login elements has been added

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,22 @@ import React, {Component} from 'react';
   render() {
      return(
 	 <div className="Login">
-      <p>  Login </p>
+      <div className="credentialinput"> 
+          <p>  Login </p>
+       <br/>
+      <p> Login </p>
+      <input type='text'  name='login'  onChange={this.changeLogin}/> 
+      <br/>
+      <p> Password </p>
+      <input type='text'   name='pass'   onChange={this.changePass} />
+      </div>
+      <div className='loginButton' >
+      <br/>
+      <br/>
+      <button name='login'  onClick={this.sendLogin} >
+      Login
+      </button>
+      </div>
 	  </div>
 )
 }
